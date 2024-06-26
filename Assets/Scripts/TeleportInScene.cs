00e53aa57fa2638db09f6b200d03b5f825b30b7b
Titaliukas <rimtitas@gmail.com>
2024-06-26 20:50:02 +0300

    phone animation

--- Assets/Scripts/TeleportInScene.cs
+++ Assets/Scripts/TeleportInScene.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.TimeZoneInfo;
+using UnityEngine.SceneManagement;
 
 public class TeleportInScene : MonoBehaviour
 {
@@ -8,13 +10,25 @@ public class TeleportInScene : MonoBehaviour
     public Transform player, destination;
     public GameObject playerg;
 
+    public Animator transition;
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player")) { }
         {
-            playerg.SetActive(false);
-            player.position = destination.position;
-            playerg.SetActive(true);
+            StartCoroutine(LoadScene());
         }
+    }
+    IEnumerator LoadScene()
+    {
+        transition.SetTrigger("Start");
+        Debug.Log("Loading scene");
+        Debug.Log("triger0");
+        yield return new WaitForSeconds(4);
+        Debug.Log("triger1");
+        playerg.SetActive(false);
+        player.position = destination.position;
+        Debug.Log("triger2");
+        playerg.SetActive(true);
     }
 }
