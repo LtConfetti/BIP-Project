3e00e9479cd1a8b16dc362eb05e84ae2cf6543d0
0-0Commando <illiafilipas@gmail.com>
2024-06-26 10:59:35 +0300

    Boss

--- Assets/Scripts/Boss/HPBoss.cs
+++ Assets/Scripts/Boss/HPBoss.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class HPBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public int bossHealth = 10;
+    private int currentBossHealth = 1;
+    public int bossDamage = 1;
     void Start()
     {
-        
+        currentBossHealth = bossHealth;
     }
 
     // Update is called once per frame
-    void Update()
+    public void TakeFromPlayerDamage(int damage)
     {
-        
+        currentBossHealth -= damage;
+        if (currentBossHealth <= 0)
+        {
+            Die();
+        }
     }
+
+    void Die()
+    {
+        Debug.Log("Enemy died");
+        Destroy(gameObject);
+    }
+
+
 }
